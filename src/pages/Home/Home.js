6f66cffa6fe7f0ec430d35d244236fb7d90d8819
Igor Josiel <igorjosielvitalino@gmail.com>
2024-08-05 23:41:49 -0300

    Ajusta a tela inicial do projeto.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,11 @@
+import { useState } from "react";
+
 // CSS
 import styles from "./Home.module.css";
 
 // hooks
 import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useNavigate, Link } from "react-router-dom";
-
-// react
-import { useState } from "react";
 
 // components
 import PostDetail from "../../components/PostDetail";
@@ -30,17 +29,20 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
-      <h1>Veja os nossos posts mais recentes</h1>
+      <h2 className={styles.title_posts}>Veja os nossos posts mais recentes</h2>
+      
       <form className={styles.search_form} onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Ou busque por tags..."
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button className="btn btn-dark">Pesquisardd</button>
+        <button className="btn btn-dark">Pesquisar</button>
       </form>
+
       <div className="post-list">
         {loading && <p>Carregando...</p>}
+
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts</p>
@@ -49,6 +51,7 @@ const Home = () => {
             </Link>
           </div>
         )}
+
         {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
       </div>
     </div>
