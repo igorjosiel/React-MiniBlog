1ce22186926ab96dc56ee7fae6a036eb1f597cb9
Igor Josiel <igorjosielvitalino@gmail.com>
2024-08-21 14:49:11 -0300

    Acrescenta cabeçalho para a tabela de coisas a fazer.

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -6,14 +6,23 @@ import styles from "./Todo.module.css";
 
 const Todo = () => {
   return (
-    <div className={styles.todo}>
-      <h2>A Fazer</h2>
-      <p>Gerencie as suas tarefas a fazer</p>
+    <>
+      <div className={styles.todo}>
+        <h2>A Fazer</h2>
+        <p>Gerencie as suas tarefas a fazer</p>
 
-      <Link to="/" className="btn">
-        Nova Tarefa
-      </Link>
-    </div>
+        <Link to="/" className="btn">
+          Nova Tarefa
+        </Link>
+
+        <div className={styles.todo_header}>
+          <span>Tarefa</span>
+          <span>Prioridade</span>
+          <span>Esforço</span>
+          <span>Ações</span>
+        </div>
+      </div>
+    </>
   );
 };
 
